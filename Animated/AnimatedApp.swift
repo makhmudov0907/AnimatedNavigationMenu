391688fb90419f4ca7622529165fb303efbcf33e
makhmudov0907 <71522647+makhmudov0907@users.noreply.github.com>
2022-06-06 17:10:07 +0500

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Mirshod Makhmudov on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animated
+//
+//  Created by Mirshod Makhmudov on 06/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
